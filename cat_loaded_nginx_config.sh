2a33525c3b56f0f82b1b7ca25822d32a0f7df6b8
Florian Kempenich <florian.kempenich@blip.pt>
2017-09-15 19:40:08 +0100

    Add comment on `./cat...` script

--- cat_loaded_nginx_config.sh
+++ cat_loaded_nginx_config.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# `-T` option is there to ensure `docker exec` doesn't output CRLF (line endings)
+# That makes possible to pipe the output of this script to a file:
+#
+#     ./cat_loaded_nginx_config.sh > my_current_config.conf
+#
 docker-compose exec -T proxy cat /etc/nginx/nginx.conf
