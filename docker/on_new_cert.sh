78b0e55968fe479deb9641afcadeb903a43a223f
Florian Kempenich <florian.kempenich@blip.pt>
2017-09-16 01:00:39 +0100

    Update certificate detection

--- docker/on_new_cert.sh
+++ docker/on_new_cert.sh
@@ -4,14 +4,16 @@ LETSENCRYPT_CERT_DIR=/https/letsencrypt/live/
 
 while true
   do
-    inotifywait -r -eCREATE $LETSENCRYPT_CERT_DIR > /output 2> /err
-    echo
-    echo "NEW CERTIFICATES DETECTED !!!!!!!!!!"
-    echo "NEW CERTIFICATES DETECTED !!!!!!!!!!"
-    echo "NEW CERTIFICATES DETECTED !!!!!!!!!!"
-    echo "NEW CERTIFICATES DETECTED !!!!!!!!!!"
-    echo
-    $@
+    # Block until change is detected (or error)
+    inotifywait -r -eCREATE $LETSENCRYPT_CERT_DIR
+    if [ $? == '0' ]; then
+        # New certificates detected
+        echo "NEW CERTIFICATES DETECTED !!!!!!!!!!"
+        $@
+    else
+        # Error: Retry in 1s
+        sleep 1
+    fi
   done
 
 echo "Exited the loop !!"
