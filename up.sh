371d66b0cdd917e643531cf075c8f9f1c671499d
Florian Kempenich <Shockn745@gmail.com>
2017-10-01 16:02:18 +0100

    Check required folder structure is present on host

--- up.sh
+++ up.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-function check_directory_exist {
-  docker run --rm -v"/:/hostroot" ubuntu ls /hostroot$2 > /dev/null 2>&1
+function check_exist {
+  docker run --rm -v"/:/hostroot" alpine ls /hostroot$2 > /dev/null 2>&1
   if ! [ "$?" == 0 ]; then
     echo ""
-    echo "$1 \`$2\` could not be found."
-    echo "Please check config!"
+    echo "$1: \`$2\` could not be found."
+    echo "==> Please check config!"
     echo ""
     exit 1
   fi
 }
 
+# Load configuration
 # Todo: Maybe make a bit more secure than that .... let's see
 source ./config.env
+check_exist "Service Configuration directory" $DIR_CONFIG
+check_exist "\`services.conf\`" $DIR_CONFIG/services.conf
+check_exist "Webroot directory" $DIR_WEBROOT
+check_exist "Certificates base directory" $DIR_CERTIFICATES
 
-
-
-check_directory_exist Root /
-check_directory_exist Etc /etc
-check_directory_exist "Stupid thing" asdf
 
