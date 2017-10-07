a7253d35001341e6121c163b483cfdf35511f5b1
Florian Kempenich <Shockn745@gmail.com>
2017-10-07 12:23:56 +0100

    Update location of thegateconfig

--- experimentations.sh
+++ experimentations.sh
@@ -1,58 +1,102 @@
 #!/bin/bash
 
 #Experimentation
-dir=/https/letsencrypt/
-dir_noslash=/https/letsencrypt
+# dir=/https/letsencrypt/
+# dir_noslash=/https/letsencrypt
 
-file=./live/the-ecosystem.xyz/fullchain.pem
-file_nodot=live/the-ecosystem.xyz/fullchain.pem
+# file=./live/the-ecosystem.xyz/fullchain.pem
+# file_nodot=live/the-ecosystem.xyz/fullchain.pem
 
 
-function full_path {
-    echo ${1%/}/${2#./}
+# function full_path {
+#     echo ${1%/}/${2#./}
+# }
+
+# echo $(full_path $dir         $file      )
+# echo $(full_path $dir_noslash $file      )
+# echo $(full_path $dir         $file_nodot)
+# echo $(full_path $dir_noslash $file_nodot)
+
+# FILE_ABS=$(full_path $dir $file)
+
+# echo
+# echo "Results:"
+# echo "FILE_ABS  ->  $FILE_ABS"
+# echo
+# echo "\$(dirname \$FILE_ABS) -> $(dirname $FILE_ABS)"
+
+# hey=/https/letsencrypt/live/the-ecosystem.xyz/fullchain.pem
+# hey2=/https/letsencrypt/live/the-ecosystem.xyz/asdf.pem
+# boom=/https/letsencrypt/live/another-dir/fullchain.pem
+
+# function same_basedir {
+#     if [ "$(dirname $1)" == "$(dirname $2)" ]; then
+#         echo true
+#     else
+#         echo false
+#     fi
+# }
+
+# echo "echo same_basedir \$hey \$hey2 ==> "$(same_basedir $hey $hey2)
+# echo "echo same_basedir \$hey \$boom ==> "$(same_basedir $hey $boom)
+# echo "echo same_basedir \$hey2 \$boom ==> "$(same_basedir $hey2 $boom)
+
+# if [ "$(same_basedir $hey $hey2)" == true ]; then
+#     echo IT WORKS
+# fi
+
+# function watch_file_and_exec_cmd {
+#     file=$1
+#     cmd=${@:2}
+
+#     echo "file: $file"
+#     echo "cmd: $cmd"
+#     echo "cmd: and this is a command"
+# }
+
+# watch_file_and_exec_cmd ./hello_this_is_a_file and this is a command
+
+
+function check_variable_set {
+    varname=$1
+    var=${!varname}
+
+    other=$(echo $"DIR_CONFIG")
+
+    echo "Varname: |$varname|"
+    echo "Var: |$var|"
+    echo "Other: |$other|"
+
+    # if [ -z "$var" ]; then
+    #     gateconfig_error "Variable NOT SET ==> $varname"
+    #     exit 1;
+    # else
+    #     echo "Varname: |$varname|"
+    #     echo "Var: |$var|"
+    # fi
 }
+DIR_CONFIG="hello"
+check_variable_set "DIR_CONFIG"
 
-echo $(full_path $dir         $file      )
-echo $(full_path $dir_noslash $file      )
-echo $(full_path $dir         $file_nodot)
-echo $(full_path $dir_noslash $file_nodot)
 
-FILE_ABS=$(full_path $dir $file)
 
-echo
-echo "Results:"
-echo "FILE_ABS  ->  $FILE_ABS"
-echo
-echo "\$(dirname \$FILE_ABS) -> $(dirname $FILE_ABS)"
 
-hey=/https/letsencrypt/live/the-ecosystem.xyz/fullchain.pem
-hey2=/https/letsencrypt/live/the-ecosystem.xyz/asdf.pem
-boom=/https/letsencrypt/live/another-dir/fullchain.pem
 
-function same_basedir {
-    if [ "$(dirname $1)" == "$(dirname $2)" ]; then
-        echo true
-    else
-        echo false
-    fi
-}
 
-echo "echo same_basedir \$hey \$hey2 ==> "$(same_basedir $hey $hey2)
-echo "echo same_basedir \$hey \$boom ==> "$(same_basedir $hey $boom)
-echo "echo same_basedir \$hey2 \$boom ==> "$(same_basedir $hey2 $boom)
 
-if [ "$(same_basedir $hey $hey2)" == true ]; then
-    echo IT WORKS
-fi
 
-function watch_file_and_exec_cmd {
-    file=$1
-    cmd=${@:2}
 
-    echo "file: $file"
-    echo "cmd: $cmd"
-    echo "cmd: and this is a command"
-}
 
-watch_file_and_exec_cmd ./hello_this_is_a_file and this is a command
+
+
+
+
+
+
+
+
+
+
+
+
 
--- up.sh
+++ up.sh
@@ -9,12 +9,41 @@ docker rm the-gate > /dev/null 2>&1
 # END - DEBUG  #
 # TODO: Remove #
 ################
+GATECONFIG=$HOME/.thegateconfig
+
+function check_config_exist {
+    if [ ! -f $GATECONFIG ]; then
+        gateconfig_error "NOT FOUND"
+        exit 1;
+    fi
+}
+function check_variable_set {
+    varname=$1
+    var=${!varname}
+
+    if [ -z "$var" ]; then
+        gateconfig_error "Variable NOT SET ==> $varname"
+        exit 1;
+    fi
+}
+function gateconfig_error {
+    error_message=$1
+    echo "The base configuration for The-Gate could not be found."
+    echo "Please ensure the config exist and is properly formatted:"
+    echo
+    echo "    CONFIG LOCATION: $GATECONFIG"
+    echo "    ERROR: $error_message"
+    echo
+    echo "For more info on how to create the config, refer to README.md."
+}
+
 function cleanup_trailing_slash { echo ${1%/}; }
 function cleanup_leading_dotslash { echo ${1#./}; }
 function full_path {
     echo $(cleanup_trailing_slash $1)/$(cleanup_leading_dotslash $2)
 }
-function check_exist {
+
+function check_exist_on_host {
     # Mounts the root on a docker image, and does the check on the image.
     #
     # That allow to check if file is present on Docker HOST,
@@ -35,8 +64,14 @@ function check_exist {
 ######################
 
 ## Load configuration ###########################################
-# Todo: Maybe make a bit more secure than that .... let's see
-source ./config.env
+check_config_exist
+source $GATECONFIG
+
+check_variable_set "DIR_CONFIG"
+check_variable_set "DIR_WEBROOT"
+check_variable_set "DIR_CERTIFICATES"
+check_variable_set "FILE_CERT"
+check_variable_set "FILE_PRIVKEY"
 
 DIR_CONFIG=$(cleanup_trailing_slash $DIR_CONFIG)
 DIR_WEBROOT=$(cleanup_trailing_slash $DIR_WEBROOT)
@@ -53,10 +88,10 @@ FILE_PRIVKEY_ABS_IN_CONTAINER=$(full_path $DIR_CERTIFICATES_IN_CONTAINER $FILE_P
 
 
 ## Check config present on host #################################
-check_exist "Service Configuration directory" $DIR_CONFIG
-check_exist "\`services.conf\`" $DIR_CONFIG/services.conf
-check_exist "Webroot directory" $DIR_WEBROOT
-check_exist "Certificates base directory" $DIR_CERTIFICATES
+check_exist_on_host "Service Configuration directory" $DIR_CONFIG
+check_exist_on_host "\`services.conf\`" $DIR_CONFIG/services.conf
+check_exist_on_host "Webroot directory" $DIR_WEBROOT
+check_exist_on_host "Certificates base directory" $DIR_CERTIFICATES
 ## End - Check config present on host ###########################
 
 
