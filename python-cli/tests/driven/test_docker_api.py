539fa8e1e25819274605cf0dd69db5e95bd0b61a
Florian Kempenich <Flori@nKempenich.com>
2018-06-20 14:47:11 +0100

    Clean up fixture process

--- python-cli/tests/driven/test_docker_api.py
+++ python-cli/tests/driven/test_docker_api.py
@@ -7,38 +7,41 @@ from thegate.driven.docker_api import DockerApi
 
 
 @pytest.fixture
-def docker_api():
-    return DockerApi()
+def containers_run_mock():
+    return MagicMock(name='containers.run')
+
+
+@pytest.fixture
+def docker_api(containers_run_mock):
+    def patch_docker_module(containers_run_mock: MagicMock):
+        patched_docker_module = patch('thegate.driven.docker_api.docker')
+
+        docker_mock = patched_docker_module.start()
+        docker_mock\
+            .from_env.return_value\
+            .containers\
+            .run = containers_run_mock
+
+        return patched_docker_module
+
+    def unpatch_docker_module(patched_docker_module):
+        patched_docker_module.stop()
+
+    patched_docker_module = patch_docker_module(containers_run_mock)
+    yield DockerApi()
+    unpatch_docker_module(patched_docker_module)
 
 
 class TestRun:
-    @patch('docker.from_env')
-    def test_simple_run(self, from_env_mock, docker_api):
-        # Given: 'run' method is mocked
-        run_mock = MagicMock(name='containers.run')
-        from_env_mock.return_value\
-            .containers\
-            .run = run_mock
-
-        # When: Calling 'run'
+    def test_simple_run(self, docker_api, containers_run_mock):
         docker_api.run("ubuntu:latest", "ls")
+        containers_run_mock.assert_called_with("ubuntu:latest", "ls")
 
-        # Then: Call has been relayed to 'docker' module
-        run_mock.assert_called_with("ubuntu:latest", "ls")
+    def test_run_with_volume_single(self, docker_api, containers_run_mock):
+        docker_api.run("ubuntu:latest", "ls",
+                       volumes=("/home:/mounted_home:rw",))
 
-    @patch('docker.from_env')
-    def test_run_with_volume_single(self, from_env_mock, docker_api):
-        # Given: 'run' method is mocked
-        run_mock = MagicMock(name='containers.run')
-        from_env_mock.return_value\
-            .containers\
-            .run = run_mock
-
-        # When: Calling 'run'
-        docker_api.run("ubuntu:latest", "ls", volumes=("/home:/mounted_home:rw",))
-
-        # Then: Call has been relayed to 'docker' module
-        run_mock.assert_called_with(
+        containers_run_mock.assert_called_with(
             "ubuntu:latest",
             "ls",
             volumes={
